55e197850302e467461b10c69ca7fb62600b188b
heyfey <tsungtsohsieh@gmail.com>
2018-08-22 01:15:06 +0800

    refactoring --make it more readable

--- autoBookseatTKB/autoBookseatTKB.py
+++ autoBookseatTKB/autoBookseatTKB.py
@@ -24,21 +24,25 @@ class AutoBookseatTKB:
         self.wait = WebDriverWait(self.driver, 60)
 
     def login(self):
-        self.driver.find_element_by_id("id").clear()
-        self.driver.find_element_by_id("id").send_keys(self.settings['id'])
+        element = self.driver.find_element_by_id("id")
+        element.clear()
+        element.send_keys(self.settings['id'])
 
-        self.driver.find_element_by_id("pwd").clear()
-        self.driver.find_element_by_id("pwd").send_keys(self.settings['password'])
+        element = self.driver.find_element_by_id("pwd")
+        element.clear()
+        element.send_keys(self.settings['password'])
 
-        self.driver.find_element_by_id("logininputcode").click()
-        self.driver.find_element_by_id("logininputcode").clear()
+        element = self.driver.find_element_by_id("logininputcode")
+        element.click()
+        element.clear()
         code = self.driver.execute_script("return LonginSecurityCode;")
-        self.driver.find_element_by_id("logininputcode").send_keys(code)
+        element.send_keys(code)
 
         self.click_send()
 
     def click_send(self):
-        self.driver.find_element_by_link_text(u"送出").click()
+        element = self.driver.find_element_by_link_text(u"送出")
+        element.click()
 
     def wait_until_tomorrow(self):
         """Wait until tommorow 00:00:01 am"""
@@ -57,40 +61,51 @@ class AutoBookseatTKB:
         self.driver.refresh()
 
     def select_class(self):
-        self.driver.find_element_by_id("class_selector").click()
-        Select(self.driver.find_element_by_id("class_selector")).select_by_index(
-            self.settings['classIndex'])
-        self.driver.find_element_by_id("class_selector").click()
+        element = self.driver.find_element_by_id("class_selector")
+        element.click()
+        Select(element).select_by_index(self.settings['classIndex'])
+        element.click()
 
     def enter_securitycode(self):
-        self.driver.find_element_by_id("userinputcode").click()
-        self.driver.find_element_by_id("userinputcode").clear()
+        element = self.driver.find_element_by_id("userinputcode")
+        element.click()
+        element.clear()
         code = self.driver.execute_script("return SecurityCode;")
-        self.driver.find_element_by_id("userinputcode").send_keys(code)
+        element.send_keys(code)
 
     def select_location(self):
         location_value = self.location_list[self.settings['location']]
-        element = self.wait.until(EC.presence_of_element_located((
-            By.CSS_SELECTOR, 
-            "option[value=%s]" % location_value)))
-        self.driver.find_element_by_id("branch_selector").click()
-        Select(self.driver.find_element_by_id("branch_selector")).select_by_value(location_value)
-        self.driver.find_element_by_id("branch_selector").click()
+        element = self.wait.until(
+            EC.presence_of_element_located((
+                By.CSS_SELECTOR, 
+                "option[value=%s]" % location_value
+            ))
+        )
+        element = self.driver.find_element_by_id("branch_selector")
+        element.click()
+        Select(element).select_by_value(location_value)
+        element.click()
 
     def select_date(self):
-        element = self.wait.until(EC.presence_of_element_located((
-            By.CSS_SELECTOR, 
-            "option[value='%d-%02d-%02d']" % (self.date.year, 
-                                              self.date.month, 
-                                              self.date.day))))
-        self.driver.find_element_by_id("date_selector").click()
-        Select(self.driver.find_element_by_id("date_selector")).select_by_value(str(self.date))
-        self.driver.find_element_by_id("date_selector").click()
+        element = self.wait.until(
+            EC.presence_of_element_located((
+                By.CSS_SELECTOR, 
+                "option[value='%d-%02d-%02d']" % (self.date.year, 
+                                                  self.date.month, 
+                                                  self.date.day)
+            ))
+        )
+        element = self.driver.find_element_by_id("date_selector")
+        element.click()
+        Select(element).select_by_value(str(self.date))
+        element.click()
 
     def select_sessions(self):
-        element = self.wait.until(EC.presence_of_element_located((
-            By.ID, "session_time_div")))
-        self.driver.find_element_by_name("session_time").click()
+        element = self.wait.until(
+            EC.presence_of_element_located((By.ID, "session_time_div"))
+        )
+        element = self.driver.find_element_by_name("session_time")
+        element.click()
         for i in self.settings['sessions']:
             if self.driver.find_elements_by_xpath('//input[@value="%d"]' % i):
                 self.driver.find_element_by_xpath('//input[@value="%d"]' % i).click()
